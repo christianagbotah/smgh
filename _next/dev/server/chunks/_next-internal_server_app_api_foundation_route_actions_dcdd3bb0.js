a6a8eb16375c3581ff293db7b505100fab1e9271
Z User <z@container>
2026-04-18 00:45:25 +0000

    24515c99-0f8d-42c8-b9dc-1b050d0d6128

---
+++ _next/dev/server/chunks/_next-internal_server_app_api_foundation_route_actions_dcdd3bb0.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/.next-internal/server/app/api/foundation/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=_next-internal_server_app_api_foundation_route_actions_dcdd3bb0.js.map
