be619480f4429269641d7a27a20a2e0a74ab1e54
Z User <z@container>
2026-04-20 01:16:29 +0000

    build: rebuild .next with debug endpoint and latest code

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1vJAP4miyJ3A3Zakmhd5casuGpe+SlUbcSnufi2F4bs=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"MYFpqSEIsi2WT0M2f56AihaxYV0nyQiFccYiYGuMziY=\"\n}"
--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -263,6 +263,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../src/app/api/debug/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/debug">> = Specific
+  const handler = {} as typeof import("../../src/app/api/debug/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../src/app/api/donations/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/donations">> = Specific
